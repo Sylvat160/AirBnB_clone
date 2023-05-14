71b344824d8aa71257385a2d99726ebe1aa7e1e3
Sylvain T <sylvaintagnabou@gmail.com>
2023-05-14 15:19:26 +0000

    ✅✅models

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines the state model
+"""
+from .base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel
+    """
+    name = ""
